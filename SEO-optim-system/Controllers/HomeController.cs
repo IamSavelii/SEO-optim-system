dd2d9765f2aad5c54c2d95c91eb894e8bae2041e
Savelii Kryuchkov <saveliikr@yandex.ru>
2019-04-24 21:26:36 +0700

    Добавьте файлы проекта.

---
+++ SEO-optim-system/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEO_optim_system.Models;
+
+namespace SEO_optim_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Check_site()
+        {
+
+
+
+            return View();
+        }
+        public IActionResult Data()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+        public IActionResult Directory()
+        {
+            return View();
+        }
+    }
+}
---
+++ SEO-optim-system/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEO_optim_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
