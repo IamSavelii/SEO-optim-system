4a471bb657c36b63a0bec3427a8296eb5648883e
Savelii Kryuchkov <saveliikr@yandex.ru>
2019-04-25 06:41:14 +0700

    add method Yandex SQI

--- SEO-optim-system/Controllers/HomeController.cs
+++ SEO-optim-system/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SEO_optim_system.Models;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Network.Default;
 
 namespace SEO_optim_system.Controllers
 {
@@ -24,13 +27,12 @@ namespace SEO_optim_system.Controllers
         {
             return View();
         }
+
         public IActionResult Check_site()
         {
-
-
-
             return View();
         }
+        
         public IActionResult Data()
         {
             return View();
@@ -42,6 +44,36 @@ namespace SEO_optim_system.Controllers
         public IActionResult Directory()
         {
             return View();
+        }
+
+        public JsonResult GetSpam() {
+            return Json(DateTime.Now);
+        }
+
+        public async Task<JsonResult> GetParametrs(string url) {
+            return Json(new
+            {
+                YandexSQI = await YandexSQI(url),
+
+                HUI = "HUI"
+            });
+
+        }
+
+        public async Task<string> YandexSQI(string url)
+        {
+            var adress = "https://seobudget.ru/freechecker/site/" + url;
+            var requester = new HttpRequester(adress);
+            requester.Headers["User-Agent"] = "Dinamokid";
+            var configuration = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader().WithCss().WithCookies());
+            var document = await context.OpenAsync(adress);
+            var result = document.QuerySelector("span.success").TextContent;
+            return await Delete(result);
+        }
+
+        public async Task<string> Delete(string text){
+            return String.Join("", text.Split('.')).Trim();
         }
     }
 }
--- SEO-optim-system/Program.cs
+++ SEO-optim-system/Program.cs
@@ -14,6 +14,7 @@ namespace SEO_optim_system
     {
         public static void Main(string[] args)
         {
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
