de8ea511f8e681fc9470b1cc86c333bb62e909bd
Savelii Kryuchkov <saveliikr@yandex.ru>
2019-04-25 21:41:07 +0700

    add parse and fix check_site

--- SEO-optim-system/Controllers/HomeController.cs
+++ SEO-optim-system/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using AngleSharp;
 using AngleSharp.Network.Default;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SEO_optim_system.Models;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -50,14 +52,16 @@ namespace SEO_optim_system.Controllers
 
         public async Task<JsonResult> GetParametrs(string url)
         {
+            var trstspm = await GetSpamAndTrast(url);
             return Json(new
             {
                 site = url,
                 pageTitle = await GetTitle(url),
                 pageDecription = await GetDescription(url),
-                YandexSQI = await YandexSQI(url),
+                SQI = await YandexSQI(url),
                 pageSpeedDesktop = await GetPageSpeedDesktop(url),
-                date = DateTime.Now
+                trast = trstspm[0],
+                spam = trstspm[1]
             });
 
         }
@@ -118,10 +122,43 @@ namespace SEO_optim_system.Controllers
             var configuration = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
             var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
             var document = await context.OpenAsync(adress);
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(4000);
             var result = document.QuerySelector("#pagespeed_desktop > div > div.span7 > span.label.score-label.label-warning").TextContent;
             return await Delete(result);
         }
 
+        public async Task<List<string>> GetSpamAndTrast(string url)
+        {
+            List<string> trtspm = new List<string>();
+            var adress = "https://checktrust.ru/app.php?r=host/app/summary/basic&applicationKey=338bc2140502115cf718af016929edc6&host="  + url + "&parameterList=spam,trust";
+            var requester = new HttpRequester(adress);
+            requester.Headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            requester.Headers["Accept-Charset"] = "utf-8";
+            requester.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            var configuration = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var document = await context.OpenAsync(adress);
+            var json = document.QuerySelector("html").TextContent;
+            var result = JsonConvert.DeserializeObject<Rootobject>(json);
+            trtspm.Add(result.summary.trust);
+            trtspm.Add(result.summary.spam);
+            return trtspm;
+        }
+
+    }
+    public class Rootobject
+    {
+        public bool success { get; set; }
+        public Summary summary { get; set; }
+        public int hostLimitsBalance { get; set; }
+    }
+
+    public class Summary
+    {
+        public string spam { get; set; }
+        public string trust { get; set; }
+        public string quality { get; set; }
     }
 }
+
+   
