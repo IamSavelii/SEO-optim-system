3ea9e14f7929a745f6c41d168d76402939d9dd8f
Savelii Kryuchkov <saveliikr@yandex.ru>
2019-04-27 00:50:26 +0700

    add favicon, toastr and fix

---
+++ SEO-optim-system/Views/Shared/_Favicons.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEO_optim_system.Views.Shared
+{
+    public class _FaviconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
